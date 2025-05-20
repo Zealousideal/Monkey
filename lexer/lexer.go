ad63dac0da8061fd931858f5bbad90ae904f331d
Zealousideal <desinoy2018@gmail.com>
2025-05-21 00:51:27 +0530

    v2

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -5,23 +5,25 @@ import "Monkey/token"
 type Lexer struct {
 	input        string
 	position     int  // current position in input (points to current char)
-	readPosition int  //current reading position in input (after current char)
-	ch           byte //current char under examination
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
 }
 
-func New(input string) *Lexer { //Will return a struct of type lexer
+func New(input string) *Lexer {
 	l := &Lexer{input: input}
-	l.readChar()
+	l.readChar() // Read a character from the input string
 	return l
 }
 
+// Reads the characer and gives ch to lex
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
-		l.ch = 0 // 0 is ASCII code for NUL
+		l.ch = 0 // ASCII for NUL
 	} else {
-		l.ch = l.input[l.readPosition]
+		l.ch = l.input[l.readPosition] // Read a character from the input string into l.ch which is the character which is going to be lexed right now
 	}
 
+	// Increment the read Pointers by one
 	l.position = l.readPosition
 	l.readPosition += 1
 }
@@ -29,17 +31,9 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
-	l.skipWhitespace()
-
 	switch l.ch {
 	case '=':
-		if l.peekChar() == '=' {
-			ch := l.ch
-			l.readChar()
-			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
-		} else {
-			tok = newToken(token.ASSIGN, l.ch)
-		}
+		tok = newToken(token.ASSIGN, l.ch)
 
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
@@ -56,96 +50,22 @@ func (l *Lexer) NextToken() token.Token {
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
 
-	case '-':
-		tok = newToken(token.MINUS, l.ch)
-
-	case '/':
-		tok = newToken(token.SLASH, l.ch)
-
-	case '*':
-		tok = newToken(token.ASTERISK, l.ch)
-
-	case '<':
-		tok = newToken(token.LT, l.ch)
-
-	case '>':
-		tok = newToken(token.GT, l.ch)
-
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
 
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
 
-	case '!':
-		if l.peekChar() == '=' {
-			ch := l.ch
-			l.readChar()
-			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
-		} else {
-			tok = newToken(token.BANG, l.ch)
-		}
-
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
-	default:
-		if isLetter(l.ch) {
-			tok.Literal = l.readIdentifier()
-			tok.Type = token.LookupIdent(tok.Literal)
-			return tok
-		} else if isDigit(l.ch) {
-			tok.Type = token.INT
-			tok.Literal = l.readNumber()
-			return tok
-		} else {
-			tok = newToken(token.ILLEGAL, l.ch)
-		}
 	}
 
 	l.readChar()
 	return tok
-}
 
-func (l *Lexer) skipWhitespace() {
-	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
-		l.readChar()
-	}
-}
-
-func (l *Lexer) readIdentifier() string {
-	position := l.position
-	for isLetter(l.ch) {
-		l.readChar()
-	}
-	return l.input[position:l.position]
-}
-
-func (l *Lexer) readNumber() string {
-	position := l.position
-	for isDigit(l.ch) {
-		l.readChar()
-	}
-	return l.input[position:l.position]
-}
-
-func isLetter(ch byte) bool {
-	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
-}
-
-func isDigit(ch byte) bool {
-	return '0' <= ch && ch <= '9'
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
-}
-
-// This is especially helpful while operating with the `==` and `!=` oprator as without it, the `NextToken()` function will return two ASSIGN Tokens for the `==` sign and similarly for the next one
-func (l *Lexer) peekChar() byte {
-	if l.readPosition >= len(l.input) {
-		return 0
-	} else {
-		return l.input[l.readPosition]
-	}
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,67 +6,19 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `let five = 5;
-	let ten = 10;
-	
-	let add = fn(x, y) {
-		x + y
-	};
-	
-	let result = add(five, ten);
-	!-/*5;
-	5 < 10 > 5;
+	input := "=+(){},;"
 
-	if (5 < 10) {
-		return true;
-	} else {
-		return false;
-	}
-
-	10 == 10;
-	10 != 9;
-	`
-
-	// A slice of a list of expected tokens our lexer should output
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
-		{token.LET, "let"},
-		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "ten"},
-		{token.ASSIGN, "="},
-		{token.INT, "10"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "add"},
-		{token.ASSIGN, "="},
-		{token.FUNCTION, "fn"},
+		{token.PLUS, "+"},
 		{token.LPAREN, "("},
-		{token.IDENT, "x"},
-		{token.COMMA, ","},
-		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
-		{token.IDENT, "x"},
-		{token.PLUS, "+"},
-		{token.IDENT, "y"},
-		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "result"},
-		{token.ASSIGN, "="},
-		{token.IDENT, "add"},
-		{token.LPAREN, "("},
-		{token.IDENT, "five"},
 		{token.COMMA, ","},
-		{token.IDENT, "ten"},
-		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
@@ -74,14 +26,14 @@ func TestNextToken(t *testing.T) {
 	l := New(input)
 
 	for i, tt := range tests {
-		tok := l.NextToken() //See when we define this function as we have not defined it yet
+		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected = %q, got = %q", i, tt.expectedType, tok.Type)
+			t.Fatalf("tests[%d] - tokentype wrong. expected = %q, got = %q", i, tt.expectedLiteral, tok.Literal)
 		}
 
 		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected = %q, got = %q", i, tt.expectedLiteral, tok.Literal)
+			t.Fatalf("tests[%d] - literal wrong, expected = %q, got = %q", i, tt.expectedLiteral, tok.Literal)
 		}
 	}
 }
