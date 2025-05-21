bf7ff061db5d2cb052f6f136c15d7dab6e7ae83e
Zealousideal <desinoy2018@gmail.com>
2025-05-21 12:28:30 +0530

    Lexer Complete

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -15,25 +15,20 @@ func New(input string) *Lexer {
 	return l
 }
 
-// Reads the characer and gives ch to lex
-func (l *Lexer) readChar() {
-	if l.readPosition >= len(l.input) {
-		l.ch = 0 // ASCII for NUL
-	} else {
-		l.ch = l.input[l.readPosition] // Read a character from the input string into l.ch which is the character which is going to be lexed right now
-	}
-
-	// Increment the read Pointers by one
-	l.position = l.readPosition
-	l.readPosition += 1
-}
-
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
@@ -50,22 +45,111 @@ func (l *Lexer) NextToken() token.Token {
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
 
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+
+	case '<':
+		tok = newToken(token.LT, l.ch)
+
+	case '>':
+		tok = newToken(token.GT, l.ch)
+
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
 
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
 
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
 	return tok
-
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+	// The `-` is now going to be treated as a letter now. So now, you can use variable keywords like this_name.
+}
+
+// Reads the characer and gives ch to lex
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0 // ASCII for NUL
+	} else {
+		l.ch = l.input[l.readPosition] // Read a character from the input string into l.ch which is the character which is going to be lexed right now
+	}
+
+	// Increment the read Pointers by one
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
